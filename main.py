fb7e73533b4f260417ad53346930be94e6b93f44
Ayse Bat <110904026+aysebat@users.noreply.github.com>
2023-03-11 06:10:09 +0000

    Get the text using selenium

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from selenium import webdriver
 
-#set options to make browsing easier
-options = webdriver.ChromeOptions()
-options.add_argument("disable-infobars")
-options.add_argument("start-maximized") #start browser as maximized
-options.add_argument("disable-dev-shm-usage") #to avoid issues with a browser on a linux
-options.add_argument("no-sandbox") 
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_argument("disable-blink-features=AutomationControlled")
+def get_driver():
 
-#instantiate driver
-#dont need to execute the path because we are uisng repl
-driver = webdriver.Chrome()
+  #set options to make browsing easier
+  options = webdriver.ChromeOptions()
+  options.add_argument("disable-infobars")
+  options.add_argument("start-maximized") #start browser as maximized
+  options.add_argument("disable-dev-shm-usage") #to avoid issues with a browser on a linux
+  options.add_argument("no-sandbox") 
+  options.add_experimental_option("excludeSwitches", ["enable-automation"])
+  options.add_argument("disable-blink-features=AutomationControlled")
+  
+  #instantiate driver
+  #dont need to execute the path because we are uisng repl
+  driver = webdriver.Chrome(options=options)
+  driver.get("http://automated.pythonanywhere.com")
+  return driver
+
+def main():
+  driver = get_driver()
+  element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[1]")
+  return element.text
+
+print(main())
